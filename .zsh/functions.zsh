c3a9bf2276409f1e9d3bad5599b7ba5326abc2e6
Daan Klarenbeek <github@ijskoud.dev>
2023-04-18 21:13:12 +0200

    chore: update .zsh config files

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -2,3 +2,8 @@
 function colormap() {
   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
+
+# Delete Git branches that do not exist on remote anymore
+function prune-untracked-branches() {
+	git fetch -p ; git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
+}
